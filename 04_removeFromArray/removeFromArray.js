dc2f3098f209480bcf091e24eee5b44fdeaee530
stallionblade <119752878+stallionblade@users.noreply.github.com>
2022-12-23 17:15:28 -0500

    Solve TTD ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        if (array.indexOf(arg) != -1) {
+            array.splice(array.indexOf(arg),1)   
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
