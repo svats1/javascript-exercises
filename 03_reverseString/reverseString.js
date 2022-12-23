5e76e73f78168cdecbe710df5e75d4f82a6dbc32
stallionblade <119752878+stallionblade@users.noreply.github.com>
2022-12-23 11:39:19 -0500

    Solve TDD ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let input = string
+    let reverseString = ''
+    for (let i=input.length-1; i>=0; --i) {
+        reverseString += input[i]
+    }
+    return reverseString
 };
 
 // Do not edit below this line
