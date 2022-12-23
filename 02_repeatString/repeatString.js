fa425deeb7ad47dd64ae344ec05a98fea310e7d3
stallionblade <119752878+stallionblade@users.noreply.github.com>
2022-12-23 10:58:31 -0500

    Solve TDD ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let input = ""
+    if (num >= 0) {
+        for (let i=0; i<num; ++i) {
+            input += string
+        }
+        return input
+    }
+    else {
+        return "ERROR"
+    }
+    
+    
 };
 
 // Do not edit below this line
