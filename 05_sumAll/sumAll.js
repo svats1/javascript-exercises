7f96053b16ffa5c006a6d326ea73e471c38334a5
stallionblade <119752878+stallionblade@users.noreply.github.com>
2022-12-23 20:40:51 -0500

    Solve TDD ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sum = 0
+    // If both numbers are actually numbers
+    if (typeof num1 != 'string' && typeof num2 != 'string'
+    && num1 >= 0 && num2 >=0) {    
+        // If first number is smaller, do this loop
+        if (num1 < num2) {
+            for (i=num1; i<=num2; ++i) {
+                sum += i
+            }
+            return sum
+        }
+        
+        // If first number is bigger, do this loop
+        else if (num1 > num2) {
+            for (i=num1; i>=num2; --i) {
+                sum += i
+            }
+            return sum
+        }
+        
+    }
+    
+    // If any entry condition is not met
+    else {
+        return "ERROR"
+    }
+    
 };
 
 // Do not edit below this line
