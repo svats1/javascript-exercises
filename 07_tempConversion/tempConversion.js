5e17bc078cc00a79a469a7eeb3383a334e93b709
stallionblade <119752878+stallionblade@users.noreply.github.com>
2022-12-24 17:26:40 -0500

    Solve TDD ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let C = (F - 32)*5/9
+  C = parseFloat(C.toFixed(1))
+  return C
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let F = C*(9/5) + 32
+  F = parseFloat(F.toFixed(1))
+  return F
 };
 
 // Do not edit below this line
