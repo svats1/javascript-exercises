472853f72f16923682ec35d5cf4e8f2a928fe1b2
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-05 13:58:56 -0500

    Solve TDD ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    let forward = Array.from(a)
+    let punctuation = [' ', '.', ',', '!']
+    forward = forward.filter((item) => !punctuation.includes(item))
+    
+    let reverse = []
+    forward.forEach(element => {
+        reverse.unshift(element)
+    });
 
-};
+    const str1 = forward.join('').toLowerCase()
+    const str2 = reverse.join('').toLowerCase() 
+    
+    return (str1.localeCompare(str2) == 0)
+}
 
 // Do not edit below this line
 module.exports = palindromes;
