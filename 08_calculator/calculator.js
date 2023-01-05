176336cb323f1913e8ac4d96671bdbef7007545f
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-05 11:31:03 -0500

    Solve JS ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return parseInt(a) + parseInt(b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return parseInt(a) - parseInt(b)
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    sum += parseInt(a[i])
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let prod = 1;
+  for (let i = 0; i < a.length; i++) {
+    prod *= parseInt(a[i])
+  }
+  return prod
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(parseInt(a), parseInt(b))
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fac = 1
+  for (let i = parseInt(a); i > 0; i--) {
+    fac *= i
+  }
+  return fac
 };
 
 // Do not edit below this line
