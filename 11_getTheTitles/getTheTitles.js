fdec847475bcd63c7ff2ea442fb825adc9be6c7b
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-06 15:31:28 -0500

    Solve TDD ex11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = []
+    titles.push(books[0].title, books[1].title)
+    return (titles)
 };
 
 // Do not edit below this line
