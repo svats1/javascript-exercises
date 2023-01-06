82bb039bdd222c121c5ebd68d7cbf3709aa1e4af
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-06 15:17:41 -0500

    Solve TDD ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    a = parseInt(a)
+    if (a<0) return "OOPS" 
+    else {
+        let seq = [1, 1]
+        let start
+        for (let i = 2; i < a; i++) {
+            start = seq[i-1] + seq[i-2]
+            seq.push(start)
+        }
+        return (seq[a - 1])
+    }
+    
 };
 
 // Do not edit below this line
