4c308513d5a5fb64f7f6bf8d601961b59ef0d0e6
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-07 13:19:32 -0500

    Solve TDD ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let date = new Date()
+    let currentYear = date.getFullYear()
+    let ages = []
+
+    for (let i = 0; i < people.length; i++) {
+        let endYear = people[i].yearOfDeath || currentYear
+        ages.push(endYear - people[i].yearOfBirth)
+    }
+    
+    return (people[ages.indexOf(Math.max(...ages))])
+    
 
 };
 
